6e137e2051362c951a008d0683e4319d91c8c379
Rick Twohy <twohyjr@gmail.com>
2017-10-29 19:37:09 -0500

    Terrain position update

--- Twohys Game Engine/Node Files/Scene Files/PlaygroundScene1.swift
+++ Twohys Game Engine/Node Files/Scene Files/PlaygroundScene1.swift
@@ -2,20 +2,21 @@ import MetalKit
 
 class PlaygroundScene1: Scene{
     
-    var quad: Quad!
+    var object: Cube!
     var terrain: Terrain!
     override func buildScene(device: MTLDevice) {
         camera.position.z = -4
-        quad = Quad(device: device, textureName: "bright.png")
+        object = Cube(device: device)
+        object.position.y = 0.5
         
         terrain = Terrain(device: device, textureName: "grass.png")
-        terrain.position.y = -1
+        terrain.position.y = 0
         terrain.position.x = -50
         terrain.position.z = -100
         camera.rotation.x = -25
         camera.position.z = -10
         add(child: terrain)
-        add(child: quad)
+        add(child: object)
     }
     
     override func updateCamera(deltaTime: Float) {
@@ -28,12 +29,12 @@ class PlaygroundScene1: Scene{
     
     override func updateModels(deltaTime: Float) {
         //Model
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_W)){quad.position.z -= 0.05}
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_S)){quad.position.z += 0.05}
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_D)){quad.position.x += 0.05}
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_A)){quad.position.x -= 0.05}
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_Z)){quad.position.y += 0.05}
-        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_X)){quad.position.y -= 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_W)){object.position.z -= 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_S)){object.position.z += 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_D)){object.position.x += 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_A)){object.position.x -= 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_Z)){object.position.y += 0.05}
+        if(InputHandler.isKeyPressed(key: KEY_CODES.Key_X)){object.position.y -= 0.05}
     }
 }
 

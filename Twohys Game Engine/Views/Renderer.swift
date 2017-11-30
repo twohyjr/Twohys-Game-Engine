d6556f1927df959984dc8b6b2685b8ca5784d646
Rick Twohy <twohyjr@gmail.com>
2017-11-29 23:59:38 -0600

    Quick push

--- Twohys Game Engine/Views/Renderer.swift
+++ Twohys Game Engine/Views/Renderer.swift
@@ -112,12 +112,12 @@ extension Renderer: MTKViewDelegate{
         let commandBuffer = commandQueue.makeCommandBuffer()
 
         let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: passDescriptor)
-        renderCommandEncoder?.setDepthStencilState(depthStencilState)
-        renderCommandEncoder?.setFragmentSamplerState(samplerState, index: 0)
+//        renderCommandEncoder?.setDepthStencilState(depthStencilState)
+//        renderCommandEncoder?.setFragmentSamplerState(samplerState, index: 0)
 //        renderCommandEncoder?.setTriangleFillMode(.lines)
         
-        let deltaTime: Float = (Float(1.0) / Float(view.preferredFramesPerSecond))
-        scene.render(renderCommandEncoder: renderCommandEncoder!, deltaTime: deltaTime)
+//        let deltaTime: Float = (Float(1.0) / Float(view.preferredFramesPerSecond))
+//        scene.render(renderCommandEncoder: renderCommandEncoder!, deltaTime: deltaTime)
 
         renderCommandEncoder?.endEncoding()
         commandBuffer?.present(drawable)
